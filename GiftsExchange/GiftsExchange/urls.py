dd5fd228f0cd79f84dcf3c2b541bc41f8a35800d
Sabreen <sabrmbins@gmail.com>
2025-04-13 08:07:02 +0300

    update requirment

--- GiftsExchange/GiftsExchange/urls.py
+++ GiftsExchange/GiftsExchange/urls.py
@@ -23,7 +23,7 @@ urlpatterns = [
     path('', include('main.urls')),
     path('accounts/',include('accounts.urls')),
     path('events/',include('events.urls')),
-    path('wishList/',include('wishList.urls'))
+    path('wishList/',include('wishList.urls')),
 
     
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
